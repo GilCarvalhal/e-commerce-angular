73c1dd13279f14fa77153c019a102545ee9ab0bc
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-05-09 18:40:12 -0300

    pagina de detalhe do produto

---
+++ src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes-produto',
+  templateUrl: './detalhes-produto.component.html',
+  styleUrls: ['./detalhes-produto.component.css']
+})
+export class DetalhesProdutoComponent {
+
+}
--- src/app/produtos/produtos-routing.module.ts
+++ src/app/produtos/produtos-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProdutosComponent } from './produtos.component';
+import { DetalhesProdutoComponent } from './detalhes-produto/detalhes-produto.component';
 
-const routes: Routes = [{ path: '', component: ProdutosComponent }];
+const routes: Routes = [
+  { path: '', component: ProdutosComponent },
+  { path: ':id', component: DetalhesProdutoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProdutosRoutingModule { }
+export class ProdutosRoutingModule {}

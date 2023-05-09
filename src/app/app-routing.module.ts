5d5fc5627b3fb90b5deb612b45304093fc0ee781
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-05-09 13:49:44 -0300

    criacao da pagina de produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'produtos',
+    loadChildren: () =>
+      import('./produtos/produtos.module').then((m) => m.ProdutosModule),
+  },
+  { path: '', redirectTo: 'produtos', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

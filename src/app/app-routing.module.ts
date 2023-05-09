6614f1f0b812c605ec4bdaf795c28f2e71ea3adf
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-05-09 14:00:48 -0300

    pagina nao encontrada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 const routes: Routes = [
   {
@@ -8,6 +9,7 @@ const routes: Routes = [
       import('./produtos/produtos.module').then((m) => m.ProdutosModule),
   },
   { path: '', redirectTo: 'produtos', pathMatch: 'full' },
+  {path: '**', component: NaoEncontradoComponent}
 ];
 
 @NgModule({

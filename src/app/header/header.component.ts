70617a1d47198e2df42479032928c57caa19c78c
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-05-10 22:38:11 -0300

    implementando funcionalidade de adicao ao carrinho - part2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarrinhoService } from '../carrinho.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(public carrinhoService: CarrinhoService) {}
 
+  ngOnInit(): void {}
 }
